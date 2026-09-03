693f9ff2d7bdbb25da47a45a0fa7656c055d44ac
nikolaygorb <nikolay.gorb@intel.com>
2026-09-03 16:49:54 +0100

    Initial commit: project structure with pre-built rexglue SDK

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// perfectdarkremasterrecomp - ReXGlue Recompiled Project
+
+#include "generated/default/perfectdarkremasterrecomp_init.h"
+
+#include "perfectdarkremasterrecomp_app.h"
+
+REX_DEFINE_APP(perfectdarkremasterrecomp, PerfectdarkremasterrecompApp::Create)
